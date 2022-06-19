4512b2f86699dd78e2e6600f9b4815d20e97a5f9
Cody Davis <davis.r.cody@gmail.com>
2022-06-18 20:29:33 -0700

    attemping to create by association

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 'use strict'
 
 const server = require('./server')
-const { sequelize, ProfileModel } = require('./models/index')
+const { sequelize, UserModel, ProjectModel } = require('./models/index')
 
-sequelize.sync({force: true}).then(() => {
-    console.log('Connection successful')
-    ProfileModel.create({name: 'J-dawg', title: 'The dawg'})
-}).catch((err) => {
-    console.log('Unable to connect')
-})
+async function startSequelize () {
+    try {
+        await sequelize.sync({force: true})
 
+        let User = UserModel.create({name: 'J-dawg', title: 'The dawg'})
+        let Project = ProjectModel.create({title: 'React Course', description: 'A really cool app', })
+
+        await User.addProjectModel(Project)
+
+        console.log('Connection is a go!')
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+startSequelize()
 server.start()
